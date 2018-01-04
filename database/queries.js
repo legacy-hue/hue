1ab9c4fcf222139cabd6653ce8353e20e8d3ea58
James Byrd <jd3yrd@gmail.com>
2018-01-04 15:46:44 -0600

    removed 2nd body-parser; removed bcrypt error

--- database/queries.js
+++ database/queries.js
@@ -4,7 +4,6 @@ const user = (name) => {
   knex('users').where({name: name})
   .then((userData) => {
     if(userData.length){
-      console.log(userData);
       return userData;
     }else{
       console.log(name + ' does not exists in users table');
