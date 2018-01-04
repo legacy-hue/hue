6c5ee2cff9881a68ebd2eff4904222add8805e01
Joseph Strandmo <josephstrandmo@gmail.com>
2018-01-03 22:27:25 -0600

    add home.jsx and dynamically render login.jsx using react router 4

---
+++ client/src/components/home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+var Home = () => (
+	<div>
+	  <Link to="/login">login</Link>
+    <h4>Welcome to hue</h4>
+  </div>
+)
+
+export default Home;
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import { Switch, Route } from 'react-router-dom';
 
+import Home from './components/Home.jsx'
 import Login from './components/Login.jsx';
 
 class App extends React.Component {
@@ -14,17 +15,28 @@ class App extends React.Component {
 
   render() {
   	return (
-      <div>
-        <Login></Login>
-      </div>
+  <div>
+    <Switch>
+      <Route exact path="/" component={Home}/>
+      <Route exact path="/login" component={Login}/>
+    </Switch>
+  </div>
   	)
   }
 }
 
-ReactDOM.render(<App />, document.getElementById('app'));
-// ReactDOM.render((
-//   <BrowserRouter>
-//     <App />
-//   </BrowserRouter>
-// ), document.getElementById('app'))
+//ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render((
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+), document.getElementById('app'))
 
+  // <div>
+  //   <nav>
+  //     <Link to="/login">login</Link>
+  //   </nav>
+  //   <div>
+  //     <Route path="/login" component={Login}/>
+  //   </div>
+  // </div>
