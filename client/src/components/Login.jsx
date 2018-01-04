671ddc3b5e63544f5f3afcbf1b0d5f585890df23
Joseph Strandmo <josephstrandmo@gmail.com>
2018-01-03 22:02:16 -0600

    render login component

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,19 +1,30 @@
+import React from 'react';
+
 var Login = () => (
 	<div>
 	  <div>
 	    <h4>Login</h4>
 	    username: <input />
+	    <br/>
 	    password: <input />
+	    <br/>
+	    <br/>
 	    <button>login</button>
 	  </div>
-
+   
+    <br/>
 	  <a href="">Forgot your password?</a>
 
 	  <div>
 	    <h4>Signup</h4>
 	    username: <input />
+	    <br/>
 	    password: <input />
+	    <br/>
+	    <br/>
 	    <button>create account</button>
 	  </div>
   </div>
 )
+
+export default Login;
