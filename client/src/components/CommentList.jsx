d4b09d2bfefcbfec5ae307690a7be96357d4ba48
James Byrd <jd3yrd@gmail.com>
2018-01-08 17:45:54 -0600

    add entry info to top of comment page

--- client/src/components/CommentList.jsx
+++ client/src/components/CommentList.jsx
@@ -11,14 +11,20 @@ class CommentList extends React.Component {
   }
 
   componentDidMount() {
-  	this.props.getComments(this.props.entry)
+  	this.props.getComments(this.props.entry.id)
   	.then(data => this.setState({comments: data.data}))
   	.then(() => console.log(this.state.comments));
   }
 
   render () {
     return (
+    	
     	<div>
+	    	<div className="entry" class="ui message">
+	    	  <div>
+	    	    <a className = "link" href={this.props.entry.url} class="header">{this.props.entry.title}</a>
+	    	  </div>
+	    	</div>
     	  {this.state.comments.map((comment, index) => <CommentEntry key = {index} comment={comment}/>)}
     	</div>
     );
