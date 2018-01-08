e0a59523d2c2dd3bf4cc2c0ab5e97f5cb25d2ab9
Joseph Strandmo <josephstrandmo@gmail.com>
2018-01-08 16:39:50 -0600

    Add styled component to Home.jsx

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,22 +1,28 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import styles from 'styled-components';
 import { Divider, Form, Label, Button, Header, Menu } from 'semantic-ui-react'
 import EntryList from './EntryList.jsx';
 
+const Wrapper = styles.div`
+  margin: 15px 20px;
+`;
 
 var Home = (props) => (
-	<div>
+	<Wrapper>
 
-    <div class="ui three item menu center aligned">
+    <div class="ui three item menu">
       <Link to="/login"><Button class="item">Login/Signup</Button></Link>
       <Link to="/login"><Button class="item" onClick={() => props.authenticate('\logout')}>Logout</Button></Link>
       <Link to="/submit"><Button class="item">Submit</Button></Link>
     </div>
 
-    <h1 class="ui header">Welcome to hue</h1>
+    <h1 class="ui header item">Welcome to hue</h1>
     <Divider></Divider>
+
     <EntryList data = {props.data} setEntry = {props.setEntry}/>
-  </div>
+
+  </Wrapper>
 )
 
 export default Home;
