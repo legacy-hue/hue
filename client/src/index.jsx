c17115456602f0d1cc5fe3c7b39b445d67dc722c
James Byrd <jd3yrd@gmail.com>
2018-01-08 14:36:24 -0600

    moved state properties around for merge

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -24,8 +24,8 @@ class App extends React.Component {
       url: '',
       text: '',
       entries: [],
-      currentEntry: -1,
-      auth: false
+      auth: false,
+      currentEntry: -1
     }
   }
 
