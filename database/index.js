7a4d367aa41defed1d6f133f3bfb30308aac90fa
James Byrd <jd3yrd@gmail.com>
2018-01-09 12:34:17 -0600

    another try on deploying

--- database/index.js
+++ database/index.js
@@ -2,12 +2,7 @@
 
 const knex = require('knex')({
   client: 'pg',
-  connection: {
-    host : process.env.DATABASE_HOST || '127.0.0.1',
-    user : process.env.DATABASE_USER || config.dbUser,
-    password : process.env.DATABASE_PASSWORD || config.dbPass,
-    database : process.env.DATABASE_NAME ||  'hue'
-  }
+  connection: process.env.DATABASE_URL
 });
 
 knex.schema.hasTable('users').then(function(exists) {
